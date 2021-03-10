bc69dad69009c0acefc52c277afdab9bdcf86130
akapoor <anshul.kapoor@cern.ch>
2021-03-10 02:12:26 +0100

    script

--- Trainer.py
+++ Trainer.py
@@ -11,17 +11,20 @@ import glob
 import pandas as pd
 import numpy as np
 import ROOT
+import matplotlib.pyplot as plt
 
 
-# In[13]:
+# In[2]:
 
 
-TrainConfig=str(sys.argv[1])
+TrainConfig=sys.argv[1]
+#TrainConfig="TrainConfig"
 print("Importing settings from "+ TrainConfig)
 exec("from "+TrainConfig+" import *")
 
 
 # In[3]:
+
 
 
 print("Making output directory")
@@ -30,7 +33,7 @@ os.system("cp TrainConfig.py ./"+ OutputDirName+"/")
 os.system("cp Trainer.py ./"+ OutputDirName+"/")
 
 
-# In[12]:
+# In[4]:
 
 
 cat='EleType'
@@ -38,19 +41,18 @@ weight="NewWt",""
 label=["Background","Signal"]
 
 
-# In[17]:
+# In[5]:
 
 
 #Works in uproot3
 print("Making data frames")
 Sigdf=pd.DataFrame()
 Bkgdf=pd.DataFrame()
-from tqdm import tqdm
-for SigFile,SigXsecWt in tqdm(zip(SigFiles,SigXsecWts)):
+for SigFile,SigXsecWt in zip(SigFiles,SigXsecWts):
     Sigdfi = uproot.open(SigFile).get(Tree).pandas.df().query(SigCut+' & '+CommonCut)
     Sigdfi['xsecwt']=SigXsecWt
     Sigdf=pd.concat([Sigdf,Sigdfi],ignore_index=True, sort=False)
-for BkgFile,BkgXsecWt in tqdm(zip(BkgFiles,BkgXsecWts)):
+for BkgFile,BkgXsecWt in zip(BkgFiles,BkgXsecWts):
     Bkgdfi = uproot.open(BkgFile).get(Tree).pandas.df().query(BkgCut+' & '+CommonCut)
     Bkgdf=pd.concat([Bkgdf,Bkgdfi],ignore_index=True, sort=False)
 
@@ -61,6 +63,9 @@ for BkgFile,BkgXsecWt in tqdm(zip(BkgFiles,BkgXsecWts)):
 Sigdf[cat]=1
 Bkgdf[cat]=0
 
+Sigdf["Type"]="Signal"
+Bkgdf["Type"]="Background"
+
 #Reweighing
 Sigdf[weight]=1
 Bkgdf[weight]=1
@@ -69,21 +74,79 @@ df_final=pd.concat([Sigdf,Bkgdf],ignore_index=True, sort=False)
 from sklearn.model_selection import train_test_split
 EB_train, EB_test = train_test_split(df_final, test_size=testsize, random_state=RandomState, shuffle=True)
 
+TrainIndices=EB_train.index.values.tolist()
+TestIndices=EB_test.index.values.tolist()
 
-# In[16]:
+df_final.loc[TrainIndices,'Dataset'] = "Train"
+df_final.loc[TestIndices,'Dataset'] = "Test"
+
+
+# In[ ]:
+
+
+
+
+
+# In[7]:
+
+
+import seaborn as sns
+fig, axes = plt.subplots(1, 1, figsize=(10, 5))
+sns.countplot(x="Type", data=df_final, ax=axes,hue='Dataset',palette=['#432371',"#FAAE7B"])
+axes.set_title("Number of samples")
+plt.savefig(OutputDirName+"/TotalStat_TrainANDTest.png")
+    
+
+
+# In[8]:
+
+
+fig, axes = plt.subplots(1, len(features), figsize=(len(features)*5, 5))
+print("Making feature plots")
+for m in range(len(features)):
+    for i,group_df in df_final[df_final['Dataset'] == "Train"].groupby(cat):
+        group_df[features[m-1]].hist(histtype='step', bins=feature_bins[m-1], alpha=0.7,label=label[i], ax=axes[m-1], density=False, ls='-', weights =group_df[weight]/group_df[weight].sum(),linewidth=4)
+        #df_new = pd.concat([group_df, df_new],ignore_index=True, sort=False)                                                                                            
+    axes[m-1].legend(loc='upper right')
+    axes[m-1].set_xlabel(features[m-1])
+    axes[m-1].set_yscale("log")
+    axes[m-1].set_title(features[m-1]+" (Training Dataset)")
+plt.savefig(OutputDirName+"/featureplots_Training.png")
+
+
+# In[9]:
 
 
 import matplotlib.pyplot as plt
 fig, axes = plt.subplots(1, len(features), figsize=(len(features)*5, 5))
 print("Making feature plots")
-for m in tqdm(range(len(features))):
-    for i,group_df in EB_train.groupby(cat):
-        group_df[features[m-1]].hist(histtype='step', bins=feature_bins[m-1], alpha=0.7,label=label[i], ax=axes[m-1], density=False, ls='-', weights =group_df[weight],linewidth=4)
+for m in range(len(features)):
+    for i,group_df in df_final[df_final['Dataset'] == "Test"].groupby(cat):
+        group_df[features[m-1]].hist(histtype='step', bins=feature_bins[m-1], alpha=0.7,label=label[i], ax=axes[m-1], density=False, ls='-', weights =group_df[weight]/group_df[weight].sum(),linewidth=4)
         #df_new = pd.concat([group_df, df_new],ignore_index=True, sort=False)                                                                                            
     axes[m-1].legend(loc='upper right')
     axes[m-1].set_xlabel(features[m-1])
     axes[m-1].set_yscale("log")
-plt.savefig(OutputDirName+"/featureplots.png")
+    axes[m-1].set_title(features[m-1]+" (Testing Dataset)")
+plt.savefig(OutputDirName+"/featureplots_Testing.png")
+
+
+# In[11]:
+
+
+os.system("convert "+OutputDirName+"/*.png "+OutputDirName+"/mydoc.pdf")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
 
 
 # In[ ]:
